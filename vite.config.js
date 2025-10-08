39a24281b52818764dcf40c2848534e8147442c6
fiskey260 <fiskey254@outlook.com>
2025-10-08 20:58:48 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Netlify will publish from here
-  },
+  base: '/fiskey-pwa/',  // This must match your repo name
 })
