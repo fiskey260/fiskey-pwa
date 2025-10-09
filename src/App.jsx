cc81474c471f287895bbf4f5732ee4bf3398e85b
fiskey260 <fiskey254@outlook.com>
2025-10-09 18:22:31 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
+import React from "react";
 
+export default function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        fontFamily: "sans-serif",
+        flexDirection: "column",
+      }}
+    >
+      <h1>Welcome to Fiskey PWA 🚀</h1>
+      <p>Your progressive web app is live!</p>
+    </div>
+  );
+          }
