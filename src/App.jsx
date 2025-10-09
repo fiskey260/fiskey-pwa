3c481dcdcfb92f3bb86aa638b1a2b8e26827efa7
fiskey260 <fiskey254@outlook.com>
2025-10-09 19:23:46 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Fiskey PWA 🚀</h1>
+      <p>Your Progressive Web App is running perfectly!</p>
+    </div>
+  );
+}
+
+export default App;
