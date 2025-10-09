aadafedafd4943de81983136f0f060aff97b7aca
fiskey260 <fiskey254@outlook.com>
2025-10-09 18:03:22 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fiskey-pwa/',  // This must match your repo name
-})
+  base: "/", // ✅ important for Render (root domain)
+  build: {
+    outDir: "dist"
+  }
+});
